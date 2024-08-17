beb8a2dd71564775abcb37166f1e29933759a96a
TejasJadhao <tejas.jadhao@gmail.com>
2024-08-17 20:23:34 +0530

    My New Project

---
+++ src/main/java/datatypes.java
@@ -0,0 +1,16 @@
+public class datatypes {
+public static void main (String [] args){
+    byte x=2;
+    short y=3;
+    int z=4;
+    long l = 983443462;
+    float a = 1.235f;
+    double b = 0.082345;
+    char c = 'a';
+    char c1='1';
+    char c2=98;
+    boolean bl = true;
+    boolean bl2 = false;
+    System.out.println(l*a);
+}
+}
