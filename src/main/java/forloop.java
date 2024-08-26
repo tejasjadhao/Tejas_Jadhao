52702c25120e16285af439fc953566054a3e834e
TejasJadhao <tejas.jadhao@gmail.com>
2024-08-26 12:41:31 +0530

    My New Project

---
+++ src/main/java/forloop.java
@@ -0,0 +1,10 @@
+public class forloop {
+    public static void main(String[] args){
+        float i;
+        for(i=5;i>0;i=i/2)
+        {
+            System.out.println(i);
+        }
+        System.out.println("out of forloop");
+    }
+}
